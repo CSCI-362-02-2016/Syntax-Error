e6133cc3882361520792b998c72c116f9ba0ab06
rcarsonsmith <smithc2@g.cofc.edu>
2016-11-28 23:13:01 -0500

    fixed html formatting

--- TestAutomation/scripts/createReport.py
+++ TestAutomation/scripts/createReport.py
@@ -10,21 +10,21 @@ def reportCreate(testCases):
 
     report.write("<tr> \n")
     for i in range(0, 7):
-        report.write("<td>" + headers[i] + "</td> \n")
+        report.write("<th>" + headers[i] + "</th> \n")
     report.write("</tr> \n")
 
     for i in range(1, 25):
         report.write('<tr> \n')
-        report.write('<td> TestID: ' + testCases[i].testID + '</td>')
-        report.write('<td> Requirement: ' + testCases[i].testReq + '</td>')
-        report.write('<td> Component: ' + testCases[i].testComp + '</td>')
-        report.write('<td> Method: ' + testCases[i].testMethod + '</td>')
-        report.write('<td> Input: ' + testCases[i].testInput + '</td>')
-        report.write('<td> Expected Result: ' + testCases[i].testOutcome + '</td>')
+        report.write('<td>' + testCases[i].testID + '</td>')
+        report.write('<td>' + testCases[i].testReq + '</td>')
+        report.write('<td>' + testCases[i].testComp + '</td>')
+        report.write('<td>' + testCases[i].testMethod + '</td>')
+        report.write('<td>' + testCases[i].testInput + '</td>')
+        report.write('<td>' + testCases[i].testOutcome + '</td>')
         if testCases[i].testResult == True:
-            report.write('<td> Test Result: ' + 'Pass' + '</td>')
+            report.write('<td>' + 'Pass' + '</td>')
         else:
-            report.write('<td> Test Result: ' + 'Fail' + '</td>')
+            report.write('<td>' + 'Fail' + '</td>')
         report.write('<tr> \n')
 
     report.write("</table> \n </html>")
